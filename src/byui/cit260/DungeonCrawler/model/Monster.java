0e12284670e78584c3c8e2ceb67eabfb6ed49a00
Jeff <Jeff@DESKTOP-1MS316S>
2016-10-12 13:36:24 -0600

    10/6/2016 initial commit to repository

---
+++ src/byui/cit260/DungeonCrawler/model/Monster.java
@@ -0,0 +1,90 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.DungeonCrawler.model;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+/**
+ *
+ * @author Jeff
+ */
+public class Monster implements Serializable {
+    
+    private String name;
+    private String description;
+    private Double coordinates;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Double getCoordinates() {
+        return coordinates;
+    }
+
+    public void setCoordinates(Double coordinates) {
+        this.coordinates = coordinates;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 53 * hash + Objects.hashCode(this.name);
+        hash = 53 * hash + Objects.hashCode(this.description);
+        hash = 53 * hash + Objects.hashCode(this.coordinates);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Monster other = (Monster) obj;
+        if (!Objects.equals(this.name, other.name)) {
+            return false;
+        }
+        if (!Objects.equals(this.description, other.description)) {
+            return false;
+        }
+        if (!Objects.equals(this.coordinates, other.coordinates)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Monster{" + "name=" + name + ", description=" + description + ", coordinates=" + coordinates + '}';
+    }
+
+    public Monster(String name, String description, Double coordinates) {
+        this.name = name;
+        this.description = description;
+        this.coordinates = coordinates;
+    }
+    
+    
+}
